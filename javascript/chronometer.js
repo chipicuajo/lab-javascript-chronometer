a6186d9876226adca49dc3c4e797752dea72c8ac
Franco Giuntoli <franco.giuntoli@gmail.com>
2021-03-31 12:34:39 +0200

    up to twodigits

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,15 +1,37 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = 0
   }
   startClick(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++
+      // if(this.currentTime == 1){
+      //   callback()
+      // }
+    
+
+  }, 1000)
   }
   getMinutes() {
-    // ... your code goes here
+    let minutes = Math.floor(this.currentTime/60)
+    this.intervalId = setInterval(()=>{
+      if(this.currentTime % 60 == 0){
+        minutes++
+      }
+    },1000)
+    return minutes
   }
   getSeconds() {
-    // ... your code goes here
+    let seconds = this.currentTime
+    this.intervalID = setInterval(()=>{
+      seconds++
+    },1000 )
+    if(seconds>=59){
+      return seconds-60 
+    }
+    return seconds
+    
   }
   twoDigitsNumber() {
     // ... your code goes here
